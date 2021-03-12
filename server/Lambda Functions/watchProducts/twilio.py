c990890bc45ff3343f690417d6702c25f37d81a7
johnndavid <jidavid@uci.edu>
2021-03-11 22:05:31 -0800

    Developed Client and Lambda Function

--- server/Lambda Functions/watchProducts/twilio.py
+++ server/Lambda Functions/watchProducts/twilio.py
@@ -2,8 +2,6 @@ import requests
 
 def send_twilio_message(phone_number, message):
 
-    ACCOUNT_SID = "AC0388e3d447bdbab94b06987abcece99c"
-    AUTH_TOKEN = "835278c3056c79d85938b0fb42003795"
     TWILIO_URI = f"https://api.twilio.com/2010-04-01/Accounts/{ACCOUNT_SID}/Messages.json"
 
     payload = {
@@ -19,6 +17,6 @@ def send_twilio_message(phone_number, message):
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')
     except Exception as err:
-        print(f'Other error occurred: {err}') 
+        print(f'Other error occurred: {err}')
     else:
         print(f'Success! sent message to {phone_number}')
