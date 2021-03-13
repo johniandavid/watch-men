a3b6b34c7b54a26cc71e0ae1769060448dd832d1
johnndavid <jidavid@uci.edu>
2021-03-12 19:54:19 -0800

    Developed Client and Lambda Function

--- server/Lambda Functions/watchProducts/twilio.py
+++ server/Lambda Functions/watchProducts/twilio.py
@@ -1,17 +1,14 @@
 import requests
 import json
-
-with open('./config.json', 'r') as f:
-    config = json.load(f)
+from constants import ACCOUNT_SID, AUTH_TOKEN, TO_PHONE_NUMBER
 
 def send_twilio_message(phone_number, message):
-    ACCOUNT_SID = config["twilio"]["ACCOUNT_SID"]
-    AUTH_TOKEN = config["twilio"]["AUTH_TOKEN"]
+
     TWILIO_URI = f"https://api.twilio.com/2010-04-01/Accounts/{ACCOUNT_SID}/Messages.json"
 
     payload = {
-        "To": f"+1{phone_number}",
-        "From": f'{config["twilio"]["To"]}',
+        "To": f'+1{phone_number}',
+        "From": TO_PHONE_NUMBER,
         "Body": message
     }
 
@@ -25,6 +22,5 @@ def send_twilio_message(phone_number, message):
     else:
         print(f'Success! sent message to {phone_number}')
 
-
 if __name__ == "__main__":
-    send_twilio_message("2099142441","Hello")
+    send_twilio_message("2099142441","Test")
